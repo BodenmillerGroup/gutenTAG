9bbedabe99922084114363e51e0d99fdbf1bc048
JohnAbbey98 <jojohnabbey@gmail.com>
2023-05-15 11:10:43 +0000

    Plotting mean intensity trend

---
+++ test/plot_mean_intensity.R
@@ -0,0 +1,47 @@
+library(ggplot2)
+library(babynames)
+library(dplyr)
+library(tidyr)
+library(reshape2)
+
+# 1. Load inputs ####
+path_to_region03 <- "/mnt/msi_volume/processed_files/dilution_experiment/region03/mean_value.txt"
+region03 <-  read.table(path_to_region03)
+
+path_to_region04 <- "/mnt/msi_volume/processed_files/dilution_experiment/region04/mean_value.txt"
+region04 <-  read.table(path_to_region04)
+
+path_to_region05 <- "/mnt/msi_volume/processed_files/dilution_experiment/region05/mean_value.txt"
+region05 <-  read.table(path_to_region05)
+
+path_to_region06 <- "/mnt/msi_volume/processed_files/dilution_experiment/region06/mean_value.txt"
+region06 <-  read.table(path_to_region06)
+
+path_to_region07 <- "/mnt/msi_volume/processed_files/dilution_experiment/region07/mean_value.txt"
+region07 <-  read.table(path_to_region07)
+
+# 2. Create df ####
+df <- cbind(region03, region04, region05, region06, region07)
+#df["condition"] <- rownames(df)
+#df <- as_tibble(df)
+
+data <- data.frame(t(df))
+data["Moles"] <- c(1e-11, 1e-12, 1e-13, 1e-14, 1e-15)
+data["Picomoles"] <- as.character(c(10, 1, 0.1, 0.01, 0.001))
+data <- as_tibble(data)
+#reorder
+data <- data[, c("Picomoles", "uncharged_peptide_plus2", "charged_peptide_plus2", "uncharged_peptide_plus1", "charged_peptide_plus1")]
+
+
+# 3. Plot ####
+
+ggplot(data, aes(x = Picomoles)) +
+  geom_line(aes(y = uncharged_peptide_plus1, group = 1, color = "Uncharged +1")) + 
+  geom_line(aes(y = charged_peptide_plus1, group = 1, color = "Charged +1")) +
+  scale_x_discrete(limits = rev) +
+  geom_point(aes(y = uncharged_peptide_plus1)) +
+  geom_point(aes(y = charged_peptide_plus1)) +
+  scale_fill_brewer() +
+  ylab("Intensity")
+
+
