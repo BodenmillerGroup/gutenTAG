8b86714aef4443359d01ac1c861dac28012498fd
JohnAbbey98 <jojohnabbey@gmail.com>
2023-05-30 11:28:37 +0000

    Working constructVariogram function. Uses for loop to add each variogram to a list of variograms

--- analysis/variogram.R
+++ analysis/variogram.R
@@ -3,10 +3,50 @@ library(gstat)
 library(spatstat)
 library(sp)
 
-# create single marker intensity df with coordinates for every marker 
+# create single marker intensity df with coordinates for every marker
 
-Final_intensity_matrix_targeted$x <- Location_pixels$x
-Final_intensity_matrix_targeted$y <- Location_pixels$y
+
+constructVariogram <- function(df = Final_intensity_matrix_targeted, coords = Location_pixels){
+
+  names(df) <- make.names(names(df))
+  names <- colnames(df)
+  variograms <- list()
+
+  for (i in 1:ncol(df)){
+
+    marker <- data.frame(df[i])
+    colnames(marker) <- names[i]
+
+    marker$x <- coords$x
+    marker$y <- coords$y
+
+    coordinates(marker) <- ~x+y
+
+    # construct a formula from the current column name
+    formula_string <- paste0(names[i], "~1")
+    formula_object <- as.formula(formula_string)
+
+    marker_variogram <- variogram(formula_object, marker)
+
+    #marker_variogram <- variogram(names[i], marker)
+
+
+
+    # Store the result in the list
+    variograms[[names[i]]] <- marker_variogram
+
+
+
+  }
+
+  return(variograms)
+
+  }
+
+
+vgrams <- constructVariogram(df = Final_intensity_matrix_targeted, coords = Location_pixels)
+
+
 GLUT1 <- data.frame(Final_intensity_matrix_targeted$GLUT1)
 colnames(GLUT1) <- "glut1"
 GLUT1$x <- Location_pixels$x
@@ -16,14 +56,16 @@ coordinates(GLUT1) <- ~x+y
 # Create variogram
 vgram <- variogram(glut1~1, GLUT1)
 
+
+
 # Plot variogram
-plot(vgram)
+plot(vgrams$FN)
 
 # Specify an initial model
 initial_model <- vgm(psill = 1, model = "Sph", range = 100, nugget = 0.5)
 
 # Fit the model to your empirical variogram
-fitted_model <- fit.variogram(vgram, initial_model)
-plot(vgram, model = fitted_model)
+fitted_model <- fit.variogram(vgrams$GLUT1, initial_model)
+plot(vgrams$GLUT1, model = fitted_model)
 
 
