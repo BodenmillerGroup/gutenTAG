fd8f3a9eb9c4b63a8e049a8c5040d593b812048c
JohnAbbey98 <jojohnabbey@gmail.com>
2023-06-17 00:41:58 +0000

    Variogram, PCA and NMF updates

--- analysis/pca_nmf.R
+++ analysis/pca_nmf.R
@@ -1,13 +1,18 @@
-#### PCA ####
+# 1. Load Packages ####
 
 # Load packages
+{
 library(irlba)
 library(RcppML)
 library(EBImage)
 library(imager)
+library(ggplot2)
+  }
 
 # Load in functions from helper script
 source("/mnt/msi_volume/Rscripts/maldi-processing/code/helper_functions.R")
+
+# 2. PCA ####
 
 # check dimensions of
 dim(Final_intensity_matrix_targeted)
@@ -20,7 +25,7 @@ mean_temp = colMeans(Final_intensity_matrix_targeted)
 plot(mean_temp,var_temp,log="xy")
 
 # compute PCA (adjust n to set number of principal components)
-PCA_simple = prcomp_irlba(Final_intensity_matrix_targeted, n = 6)
+PCA_simple = prcomp_irlba(Final_intensity_matrix_targeted, n = 4)
 
 # summary of Principal Componants
 
@@ -31,34 +36,100 @@ Contribution = PCA_simple$rotation
 rownames(Contribution) = colnames(Final_intensity_matrix_targeted)
 View(Contribution)
 
-# scree plot
-par(las=1,bty="l")
-barplot((PCA_simple$sdev)^2/PCA_simple$totalvar*100, ylab="Proportion of variance explained")
+
+# create df for scree plot
+PCA_variance <- data.frame(Variation_Explained = (PCA_simple$sdev)^2/PCA_simple$totalvar*100,
+                           Component = colnames(PCA_simple$x))
+
+
+# ggplot scree plot
+scree_plot <- ggplot(data=PCA_variance, aes(x = as.factor(Component), y = Variation_Explained, fill = Component)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Scree Plot", x = "", y = "Variance Explained") +
+  scale_y_continuous(limit = c(0, 100))
+
+#### Save PCA ####
+
+pca_dir = paste(sample_path,"/", "PCA",sep = "")
+# if directory does not exist, create it
+if( !dir.exists(pca_dir)) {
+  dir.create(pca_dir)
+}
+print(pca_dir)
+
+# Save scree plot
+ggsave(filename = "scree_plot.png",
+       plot = scree_plot,
+       device = "png",
+       path = pca_dir)
+
 
 # plot principal components
-Plot_channel(PCA_simple$x[,1])
+Plot_channel(image = PCA_simple$x, channel_number = 3)
 
 # plot the inverse of the principal components
-Plot_channel(-PCA_simple$x[,1])
+Plot_channel(image = -PCA_simple$x, channel_number = 1)
+
+PCA_df <- data.frame(PCA_simple$x)
+
+# Save PCA images #
+for (n in 1:ncol(PCA_df)){
+
+    pca_image_path = paste(pca_dir,"/", colnames(PCA_df)[n], ".png", sep = "")
+    print(pca_image_path)
+    png(file=pca_image_path, width=1200, height=700)
+    Plot_channel(image = PCA_df, channel_number = n)
+    dev.off()
+
+}
 
 
-#### NMF ####
+# Save PCA loadings
+write.table(Contribution, file = paste(pca_dir,"/", "PCA_loadings.txt", sep = ""), sep="\t", quote = FALSE, row.names = FALSE)
+
+
+
+# 3. NMF ####
 
 # run NMF
 nmf_temp = nmf(scale(Final_intensity_matrix_targeted,center = FALSE),k = 10)
 
 # scree plot
-barplot(nmf_temp$d)
+barplot(nmf_temp$d, main = "NMF", names = seq(1:10))
 dim(nmf_temp$w)
 
 # plot factors
-Plot_channel(nmf_temp$w, 3)
+Plot_channel(nmf_temp$w, 10)
 
 # contribution of markers to factors
 Contribution_NFM = nmf_temp$h
 colnames(Contribution_NFM) = colnames(Final_intensity_matrix_targeted)
-View(t(Contribution_NFM))
+Contribution_NFM <- t(Contribution_NFM)
 
 
+#### Save NMF ####
+
+nmf_dir = paste(sample_path,"/", "NMF",sep = "")
+# if directory does not exist, create it
+if( !dir.exists(nmf_dir)) {
+  dir.create(nmf_dir)
+}
+print(nmf_dir)
+
+# Save NMF contribution
+write.table(Contribution, file = paste(nmf_dir,"/", "NMF_contribution.txt", sep = ""), sep="\t", quote = FALSE, row.names = FALSE)
+
+NMF_df <- data.frame(nmf_temp$w)
+
+# Save NMF images #
+for (n in 1:ncol(NMF_df)){
+
+  nmf_image_path = paste(nmf_dir,"/", colnames(NMF_df)[n], ".png", sep = "")
+  print(nmf_image_path)
+  png(file=nmf_image_path, width=1200, height=700)
+  Plot_channel(image = NMF_df, channel_number = n)
+  dev.off()
+
+}
 
 
--- analysis/variogram.R
+++ analysis/variogram.R
@@ -2,70 +2,169 @@ library(spatial)
 library(gstat)
 library(spatstat)
 library(sp)
+library(ggplot2)
 
-# create single marker intensity df with coordinates for every marker
-
-
+# 1. Construct Variogram function ####
 constructVariogram <- function(df = Final_intensity_matrix_targeted, coords = Location_pixels){
 
+  # Include make.names to remove problematic characters, such as spaces, +, etc.
   names(df) <- make.names(names(df))
   names <- colnames(df)
-  variograms <- list()
 
+  # Create list of variograms for each marker
+  variograms <- list()
   for (i in 1:ncol(df)){
 
     marker <- data.frame(df[i])
     colnames(marker) <- names[i]
 
+    # Assign coordinates
     marker$x <- coords$x
     marker$y <- coords$y
-
     coordinates(marker) <- ~x+y
 
-    # construct a formula from the current column name
+    # Construct a formula from the current column name
     formula_string <- paste0(names[i], "~1")
     formula_object <- as.formula(formula_string)
 
+    # Calculate variogram for the marker
     marker_variogram <- variogram(formula_object, marker)
-
-    #marker_variogram <- variogram(names[i], marker)
-
-
 
     # Store the result in the list
     variograms[[names[i]]] <- marker_variogram
-
-
 
   }
 
   return(variograms)
 
-  }
+}
 
 
+
+# Apply function to Final Intensity Matrix
 vgrams <- constructVariogram(df = Final_intensity_matrix_targeted, coords = Location_pixels)
 
+names <- colnames(Final_intensity_matrix_targeted)
 
-GLUT1 <- data.frame(Final_intensity_matrix_targeted$GLUT1)
-colnames(GLUT1) <- "glut1"
-GLUT1$x <- Location_pixels$x
-GLUT1$y <- Location_pixels$y
-coordinates(GLUT1) <- ~x+y
+# Specify an initial model to which you fit the sample variogram
+initial_model <- vgm(psill = 100, model = "Exp", range = 25, nugget = 10)
 
-# Create variogram
-vgram <- variogram(glut1~1, GLUT1)
+# Fit a variogram model to each variogram in the list
+fitted_models <- lapply(vgrams, function(vgram) fit.variogram(vgram, initial_model))
 
 
+# 2. Compute predicted semivariances for each fitted model ####
 
-# Plot variogram
-plot(vgrams$FN)
+# Initialise list
+predicted_values <- list()
+for (k in 1:length(fitted_models)){
 
-# Specify an initial model
-initial_model <- vgm(psill = 1, model = "Sph", range = 100, nugget = 0.5)
+  # Compute predicted semivariance values of fitted model at each distance
+  predicted_vals <- variogramLine(object = fitted_models[[k]], maxdist = max(vgrams[[k]]$dist))
 
-# Fit the model to your empirical variogram
-fitted_model <- fit.variogram(vgrams$GLUT1, initial_model)
-plot(vgrams$GLUT1, model = fitted_model)
+  # Add each set of predicted values to list of predicted values
+  predicted_values[[names[k]]] <- predicted_vals
+
+}
+
+
+
+
+# 3. Create dataframe for plotting all variograms as a facet plot ####
+
+# First for the empirical variogram
+
+# Add index name to the gamma column in each dataframe in the list
+for(i in seq_along(vgrams)) {
+  names(vgrams[[i]])[3] <- paste0("gamma_", i)
+}
+
+# Remove unnecessary columns
+short_vgrams <-  list()
+for (i in 1:length(vgrams)){
+
+  # obtain distance and gamma column for marker
+  short_vgram <- vgrams[[i]][c(2,3)]
+  # add to list
+  short_vgrams[[names[i]]] <- short_vgram
+
+}
+
+# Merge to dataframe by dist
+vgram_df <- Reduce(function(df1, df2) merge(df1, df2, by = "dist"), short_vgrams)
+distances <- vgram_df["dist"]
+vgram_df <- vgram_df[-1]
+colnames(vgram_df) <- names
+vgram_df <- bind_cols(distances, vgram_df)
+
+# Then for the fitted values
+
+# Add a name to the gamma column in each dataframe in the list
+for(i in seq_along(predicted_values)) {
+  names(predicted_values[[i]])[2] <- paste0("gamma_", i)
+}
+
+# Combine all dataframes in the list into a single dataframe
+gamma_df <- Reduce(function(df1, df2) merge(df1, df2, by = "dist"), predicted_values)
+
+distances <- gamma_df["dist"]
+gamma_df <- gamma_df[-1]
+colnames(gamma_df) <- names
+gamma_df <- bind_cols(distances, gamma_df)
+
+
+
+#ggplot(data = vgram_df) +
+#  geom_point(aes(x = dist, y = PD1), color = "red") +
+#  geom_line(data = gamma_df, aes(x = dist, y = PD1), color = "darkblue") +
+#  ggtitle(paste("Variogram")) +
+#  ylab("Semivariance") +
+#  xlab("Distance")
+
+
+# Reshape data to long format for ggplot2
+vgram_long <- tidyr::pivot_longer(vgram_df, cols = -dist, names_to = "feature", values_to = "gamma")
+gamma_long <- tidyr::pivot_longer(gamma_df, cols = -dist, names_to = "feature", values_to = "gamma_pred")
+
+# Combine into a single dataframe
+plot_data <- dplyr::full_join(vgram_long, gamma_long, by = c("dist", "feature"))
+
+# 4. Plot single sample variogram and fitted model.  ####
+
+# Need to specify which marker you want to plot by indexing
+ggplot(data = vgrams[[29]]) +
+  geom_point(aes(x = dist, y = gamma_29), color = "red") +
+  geom_line(data = predicted_values[[29]], aes(x = dist, y = gamma_29), color = "darkblue") +
+  ggtitle(paste("Variogram of", names(vgrams[3]))) +
+  ylab("Semivariance") +
+  xlab("Distance")
+
+
+# 5. Create facet plot for each variogram ####
+gg_variogram <- ggplot(data = plot_data) +
+  geom_point(aes(x = dist, y = gamma), color = "red") +
+  geom_line(aes(x = dist, y = gamma_pred), color = "darkblue") +
+  facet_wrap(~ feature, scales = "free_y") +
+  ggtitle("Variograms") +
+  ylab("Semivariance") +
+  xlab("Distance")
+
+
+# 6. Save ####
+variogram_dir = paste(sample_path,"/", "variograms",sep = "")
+# if directory does not exist, create it
+if( !dir.exists(variogram_dir)) {
+  dir.create(variogram_dir)
+}
+print(variogram_dir)
+
+# Save facet plot
+ggsave(filename = "variograms.png",
+       plot = gg_variogram,
+       device = "png",
+       path = variogram_dir)
+
+# save table
+write.table(vgram_df, file = paste(variogram_dir,"/", "variogram_df.txt", sep = ""), sep="\t", quote = FALSE, row.names = FALSE)
 
 
