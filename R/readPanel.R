93a75a6070e98240c15de96c41d7c49cd14e55b9
JohnAbbey98 <jojohnabbey@gmail.com>
2023-07-05 12:43:43 +0000

    Initial commit function for reading and cleaning panel

---
+++ R/readPanel.R
@@ -0,0 +1,28 @@
+#' Read .csv file panel
+#'
+#' @param path The path to the panel.
+#'
+#' @return The cleaned and ordered panel.
+#' @export
+#'
+#' @examples
+#' readPanel("/path/to/panel.csv")
+readPanel <- function(path){
+
+  # read in .csv
+  panel <- read.delim(path, sep = ",", header = TRUE, col.names = c("Name", "FeatureMass"))
+
+  # sanity check: columns are correctly named
+  if (is.numeric(panel$Name) == TRUE){
+
+    colnames(panel) = c("FeatureMass","Name")
+
+  }
+
+  # arrange feature mass in increasing order
+  panel <- dplyr::arrange(panel, panel$FeatureMass)
+
+  panel <- .cleanPanel(panel)
+
+  return(panel)
+}
