82604af8b8c75769246aad88ab172a6391998e29
JohnAbbey98 <jojohnabbey@gmail.com>
2023-07-11 11:18:04 +0000

    Initial commit for computing UMAP with basic plotting option

---
+++ R/computeUMAP.R
@@ -0,0 +1,39 @@
+#' Uniform Manifold Approximate Projection (UMAP)
+#'
+#' @param x A targeted intensity dataframe (rows: pixels, cols: features)
+#'
+#' @return object of class umap, containing at least a component with an embedding and a component with configuration settings
+#' @export
+#'
+#' @examples
+#' umap(IntensityDF)
+
+computeUMAP <- function(x, plot = FALSE){
+
+  umap_representation <- umap::umap(x)
+
+  if(plot == TRUE){
+
+    library(ggplot2)
+
+    umap_coordinates <- umap_representation$layout
+
+    df <- data.frame(UMAP1 = umap_coordinates[,1],
+                     UMAP2 = umap_coordinates[,2])
+
+    scatter_plot <- ggplot(df) +
+      geom_point(aes(x = UMAP1, y = UMAP2), alpha = 0.9) +
+      scale_color_viridis_c() +
+      theme_minimal() +
+      theme(legend.position = "right") +
+      labs(title = "UMAP on MALDI-imaging data",
+           x = "UMAP Dimension 1",
+           y = "UMAP Dimension 2")
+
+    print(scatter_plot)
+  }
+
+  return(umap_representation)
+}
+
+
