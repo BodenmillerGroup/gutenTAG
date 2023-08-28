7b44b389bafb43652eed056024f963cee28e733d
JohnAbbey98 <jojohnabbey@gmail.com>
2023-08-28 13:47:48 +0000

    Change from umap package to uwot package. Remove ggplot option

--- R/computeUMAP.R
+++ R/computeUMAP.R
@@ -2,37 +2,20 @@
 #'
 #' @param x A targeted intensity dataframe (rows: pixels, cols: features)
 #'
-#' @return object of class umap, containing at least a component with an embedding and a component with configuration settings
+#' @return a dataframe containing UMAP coordinatees
 #'
-#' @import ggplot2
-#' @importFrom umap umap
+#' @importFrom uwot umap
 #' @export
 #'
 
-computeUMAP <- function(x, plot = FALSE){
+computeUMAP <- function(x){
 
-  umap_representation <- umap::umap(x)
+  umap_representation <- uwot::umap(x)
 
-  if(plot == TRUE){
+  df <- data.frame(UMAP1 = umap_representation[,1],
+                   UMAP2 = umap_representation[,2])
 
-    umap_coordinates <- umap_representation$layout
-
-    df <- data.frame(UMAP1 = umap_coordinates[,1],
-                     UMAP2 = umap_coordinates[,2])
-
-    scatter_plot <- ggplot(df) +
-      geom_point(aes(x = UMAP1, y = UMAP2), alpha = 0.9) +
-      scale_color_viridis_c() +
-      theme_minimal() +
-      theme(legend.position = "right") +
-      labs(title = "UMAP on MALDI-imaging data",
-           x = "UMAP Dimension 1",
-           y = "UMAP Dimension 2")
-
-    print(scatter_plot)
-  }
-
-  return(umap_representation)
+  return(df)
 }
 
 
