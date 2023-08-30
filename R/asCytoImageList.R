36ea3720700ead9b0ab80477add9a8f74575b8c1
JohnAbbey98 <jojohnabbey@gmail.com>
2023-08-30 14:32:24 +0000

    Add asCytoImageList function

---
+++ R/asCytoImageList.R
@@ -0,0 +1,31 @@
+#' asCytoImageList
+#'
+#' @param final Output from getIntensityDF function
+#' @param name The name of the image
+#'
+#' @return A CytoImageList object
+#'
+#' @importFrom cytomapper CytoImageList
+#'
+#' @export
+#'
+
+asCytoImageList <- function(x, name = "test"){
+
+  dataframe <- x$IntensityDF
+  coords <- x$SpatialCoords
+
+  matrix_list <- lapply(colnames(dataframe), .curateMatrix, coords = coords, dataframe = dataframe)
+  # Create multidimensional array
+  my_array <- do.call(abind, list(matrix_list, along = 3))
+  # Create multichannel image from array
+  my_image <- Image(my_array)
+  # Create CytoImageList object from image
+  my_image <- CytoImageList(my_image)
+  # Assign image and channel names
+  names(my_image) <- name
+  cytomapper::channelNames(my_image) <- colnames(dataframe)
+
+  return(my_image)
+
+}
