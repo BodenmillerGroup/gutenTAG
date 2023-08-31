2a2ae3f5a1ffe36ec69582a1995b4931eb889d3a
JohnAbbey98 <jojohnabbey@gmail.com>
2023-08-31 07:20:34 +0000

    Clean and comment code

--- R/metapeakGeneration.R
+++ R/metapeakGeneration.R
@@ -13,13 +13,11 @@
 #'
 #' }
 #'
-#'
 #' @param x A Cardinal MSImagingExperiment object after peak detection is performed.
 #'
 #' @return A list containing information on the location, delimitations and width of metapeaks
 #'
 #' @importFrom stats convolve
-#' @import Cardinal
 #' @export
 #'
 #' @examples
