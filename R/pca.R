0d054a7cc33d844bb6c098bb13dcbb6742e3df70
JohnAbbey98 <jojohnabbey@gmail.com>
2023-09-05 19:04:13 +0000

    add set seed

--- R/pca.R
+++ R/pca.R
@@ -9,13 +9,19 @@
 #'
 #' @importFrom irlba prcomp_irlba
 #' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_bar
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 scale_y_continuous
+#' @importFrom ggplot2 theme
 #' @export
 #'
 #' @examples
 #' pca(IntensityDF, comp = 5)
 
-pca <- function(x, comp = 5, scree = FALSE, plot = FALSE){
+pca <- function(x, comp = 5, seed = 123, scree = FALSE, plot = FALSE){
 
+  set.seed(seed)
   PCA <- prcomp_irlba(x, n = comp, scale. = TRUE)
 
   if (scree == TRUE){
@@ -23,11 +29,11 @@ pca <- function(x, comp = 5, scree = FALSE, plot = FALSE){
     PCA_variance <- data.frame(Variation_Explained = (PCA$sdev)^2 / sum((PCA$sdev)^2) * 100,
                                Component = 1:comp)
 
-    scree_plot <- ggplot(data = PCA_variance, aes(x = as.factor(Component), y = Variation_Explained, fill = as.factor(Component))) +
-      geom_bar(stat = "identity") +
-      labs(title = "Scree Plot", x = "Component", y = "Variance Explained (%)") +
-      scale_y_continuous(limits = c(0, 100)) +
-      theme(legend.position = "none")
+    scree_plot <- ggplot(data = PCA_variance, ggplot2::aes(x = as.factor(Component), y = Variation_Explained, fill = as.factor(Component))) +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::labs(title = "Scree Plot", x = "Component", y = "Variance Explained (%)") +
+      ggplot2::scale_y_continuous(limits = c(0, 100)) +
+      ggplot2::theme(legend.position = "none")
     print(scree_plot)
 
   }
