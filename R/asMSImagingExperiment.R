32ca06225dc83c2e7325b9ce78c938eea0d757b6
JohnAbbey98 <jojohnabbey@gmail.com>
2023-09-25 12:56:36 +0000

    first version of conversion function

---
+++ R/asMSImagingExperiment.R
@@ -0,0 +1,36 @@
+#' Make MSImagingExperiment object
+#'
+#' @param final The output from assignMetapeaks() function
+#'
+#' @return An object of the class MSImagingExperiment from Cardinal.
+#'
+#' @import Cardinal
+#' @importFrom matter as.matrix
+#' @export
+#'
+#' @examples
+#' asMSImagingExperiment(final)
+
+asMSImagingExperiment <- function(final){
+
+  # pdata: where spatial coordinates go
+  coord <- final$SpatialCoords
+  run <- factor(rep("run0", nrow(coord)))
+  pdata <- PositionDataFrame(run=run, coord=coord)
+
+  # idata: where intensity df goes
+  idata <- t(as.matrix(final$IntensityDF)) # intensity dataframe must first be a matrix, then transposed
+
+  # ordered vector of mass locations. metapeak mz stored as mz, expected mz stored as expected_mz
+  fdata <- MassDataFrame(mz = sort(final$CorrespondenceMatrix$mz_location),
+                         expected_mz = sort(final$CorrespondenceMatrix$expected_mz_location))
+
+  # put them all together
+  out <- MSImagingExperiment(imageData=idata,
+                             featureData=fdata,
+                             pixelData=pdata)
+
+  return(out)
+
+
+}
