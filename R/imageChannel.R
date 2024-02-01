cfcc23385511f717884c984ae3e71dab552700aa
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-02-01 08:50:54 +0100

    support custom color ramps

--- R/imageChannel.R
+++ R/imageChannel.R
@@ -11,13 +11,12 @@
 #' @return An image.
 #'
 #' @importFrom imager as.cimg
-#' @importFrom imager add.colour
 #' @importFrom imager R
 #' @importFrom imager G
 #' @export
 #'
 
-imageChannel <- function(x, coords = NA, channel_number = 1, quantile_lim = 0.99, interpolate = FALSE, axes = FALSE, colna = "black") {
+imageChannel <- function(x, coords = NA, channel_number = 1, quantile_lim = 0.99, interpolate = FALSE, axes = FALSE, colna = "black", cmap = NA) {
 
   # validity checks
   .valid.imageChannel(x, coords, channel_number, quantile_lim, interpolate, axes, colna)
@@ -48,18 +47,32 @@ imageChannel <- function(x, coords = NA, channel_number = 1, quantile_lim = 0.99
   matrix_image <- imager::as.cimg(matrix_image - min(matrix_image, na.rm = TRUE))
 
   # add colour channels to the image
-  matrix_image <- imager::add.color(matrix_image, simple = TRUE)
-
-  # set red and blue channels to zero to get only green
-  R(matrix_image) <- 0
-  B(matrix_image) <- 0
-
-  if(colna == "black"){
-    bg <- 1
+  if (is.na(cmap)) {
+    # Simply fill the green channel with the intensity values
+    cRamp <- grDevices::colorRamp(c(grDevices::rgb(0, 0, 0, 1), grDevices::rgb(0, 1, 0, 1)), interpolate="linear", space="rgb")
+  } else if (cmap %in% c("magma", "inferno", "plasma", "viridis", "cividis", "rocket", "mako", "turbo")) {
+    # Use viridisLite to generate a color ramp
+    cRamp <- grDevices::colorRamp(viridisLite::virids(256, option = cmap), interpolate="linear", space="rgb")
+  } else {
+    # Assume that cmap is a color ramp
+    cRamp <- cmap
   }
 
-  if(colna == "white"){
+  # scale the intensities from [min(matrix_image), percentile(matrix_image, quantile_lim)] to [0, 1] by clipping and scaling
+  min_intensity <- min(matrix_image, na.rm = TRUE)
+  max_intensity <- quantile(matrix_image, quantile_lim, na.rm = TRUE)
+  scaled_intensities <- (pmin(matrix_image, max_intensity) - min_intensity) %/% (max_intensity - min_intensity)
+
+  # apply the specified color map
+  matrix_image <- cRamp(scaled_intensities)
+
+  # determine background color
+  if (colna == "black") {
+    bg <- 1
+  } else if (colna == "white") {
     bg <- 0
+  } else {
+    stop("colna must be either 'black' or 'white'")
   }
 
   plot(matrix_image, main = colnames(df)[channel_number],
@@ -67,6 +80,5 @@ imageChannel <- function(x, coords = NA, channel_number = 1, quantile_lim = 0.99
        xlim = c(1, max(coords$x)), ylim = c(max(coords$y), 1),
        axes = axes,
        col.na = rgb(0, 0, 0, bg)) # black background for NA
-
 
 }
