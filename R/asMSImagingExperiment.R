b094ecd61ccd5d409e3c6469992951dc18cc5e34
JohnAbbey98 <jojohnabbey@gmail.com>
2024-06-25 15:48:20 +0000

    passing R CMD check

--- R/asMSImagingExperiment.R
+++ R/asMSImagingExperiment.R
@@ -9,8 +9,8 @@
 #' @export
 #'
 #' @examples
-#' path <- system.file("extdata/Example_data.imzML", package = "maldipackage")
-#' panel_path <- system.file("extdata/ref_list.csv", package = "maldipackage")
+#' path <- system.file("extdata/Example_data.imzML", package = "gutenTAG")
+#' panel_path <- system.file("extdata/ref_list.csv", package = "gutenTAG")
 #' panel <- readPanel(path = panel_path)
 #' raw <- readMSIData(path)
 #' pre <- preProcess(raw, cores = 2)
