59b91d461cebca07a79ed957da76ce06fa018beb
JohnAbbey98 <jojohnabbey@gmail.com>
2024-10-15 16:19:53 +0000

    add computeSNR function with tests, update QC report

---
+++ R/computeSNR.R
@@ -0,0 +1,111 @@
+#' Compute SNR for each channel using Mixture Model (GMM).
+
+#' @param x Output from assignMetapeaks
+#' @param method Indicate whether to use Gaussian mixture model or Poisson mixture model
+#' @param update_correspondence Should the SNR score be added to the Correspondence Matrix (bool)
+#'
+#' @return
+#' @importFrom flexmix flexmix
+#' @importFrom flexmix FLXMRglm
+#' @export computeSNR
+#'
+#'#' @examples
+#' path <- system.file("extdata/Example_data.imzML", package = "gutenTAG")
+#' panel_path <- system.file("extdata/ref_list.csv", package = "gutenTAG")
+#' panel <- readPanel(path = panel_path)
+#' raw <- readMSIData(path)
+#' pre <- preProcess(raw, cores = 2)
+#' peaks <- peakDetection(pre, core = 2)
+#' metapeaks <- generateMetapeaks(peaks)
+#' processed <- assignMetapeaks(metapeaks, pre = pre, refList = panel)
+
+computeSNR <- function(x, method = "gaussian", update_correspondence = FALSE){
+
+  # extract  data.
+  coords <- x$SpatialCoords
+  correspondence <- x$CorrespondenceMatrix
+  df <- x$IntensityDF
+
+  # apply SNR to dataframe
+  sample_snrs <- apply(df, MARGIN = 2, FUN = .snr_calc)
+
+  # return / update correspondence matrix
+  if(update_correspondence == TRUE){
+
+    snrs <- sapply(sample_snrs, function(x) x$SNR)
+
+    # update output
+    x$CorrespondenceMatrix$snr <- snrs
+    x$SNR <- sample_snrs
+    return(x)
+
+  }else{
+
+    return(sample_snrs)
+
+  }
+}
+
+# hidden functions
+.snr_calc <- function(channel, method = "gaussian") {
+
+  # Remove zero values, if channel is all zeros insert dummy
+  nonzero_channel <- channel[channel != 0]
+
+  if (length(nonzero_channel) == 0) {
+    message("The channel contains only zeros. SNR set to 0.")
+    return(list("SNR" = 0, "clustering" = NULL, "fit" = NULL))
+  }
+
+  # Convert to a data frame
+  data <- data.frame(nonzero_channel)
+
+  # Try Gaussian or Poisson mixture
+  if (method == "gaussian") {
+    fit <- tryCatch({
+      flexmix(log2(nonzero_channel + 1) ~ 1, data = data, k = 2,
+              model = FLXMRglm(family = "gaussian"),
+              control = list(iter.max = 500, tol = 1e-6))
+    }, error = function(e) {
+      message("Error in Gaussian fitting: ", e)
+      return(NULL)
+    })
+
+  } else if (method == "poisson") {
+    fit <- tryCatch({
+      flexmix(nonzero_channel ~ 1, data = data, k = 2,
+              model = FLXMRglm(family = "poisson"),
+              control = list(iter.max = 500, tol = 1e-6))
+    }, error = function(e) {
+      message("Error in Poisson fitting: ", e)
+      return(NULL)
+    })
+
+  } else {
+    stop("Method must be either 'gaussian' or 'poisson'.")
+  }
+
+  # check if fitting worked
+  if (is.null(fit)) {
+    stop("The model failed to converge. Try different methods or check your data.")
+  }
+
+  # Get signal and noise clusters
+  clusters <- clusters(fit)
+
+  # Calculate the mean for each signal and noise clusters
+  cluster_means <- tapply(nonzero_channel, clusters, mean)
+  noise_mean <- min(cluster_means)
+  signal_mean <- max(cluster_means)
+
+  # Compute SNR (Signal-to-Noise Ratio)
+  snr <- signal_mean / noise_mean
+
+  out <- list("SNR" = snr,
+              "clustering" = clusters,
+              "fit" = fit
+              )
+
+  return(out)
+}
+
