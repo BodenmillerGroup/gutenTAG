2b9f26c8ecfb5317e4a85b495f789ce1664e4df3
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-05 15:07:49 +0100

    Merge pull request #18 from BodenmillerGroup/improveSNR

--- tests/testthat/test-computeSNR.R
+++ tests/testthat/test-computeSNR.R
@@ -11,16 +11,22 @@ test_that("computeSNR works",{
   processed <- assignMetapeaks(metapeaks, pre = pre, refList = panel)
 
   # compute Geary's C
-  cur_test <- computeSNR(processed, update_correspondence = TRUE)
+  cur_test <- computeSNR(processed, update_correspondence = TRUE, package = "mclust")
 
   # test if character in first argument throws error
   expect_error(computeSNR("processed"))
 
   # Test correspondence matrix
   expect_true("snr" %in% colnames(cur_test$CorrespondenceMatrix))
-  expect_equal(cur_test$CorrespondenceMatrix$snr, c(1, 1.46662215920315, 0, 0, 2.00872721014486, 0, 3.08641724395242,
-                                                    3.80602499772131, 3.97764446905493, 15.2477686105868, 2.40921027994661,
-                                                    1, 2.9132994473942))
+  expect_equal(cur_test$CorrespondenceMatrix$snr, c(2.07919158184831, 1.48489368089413, 0, 0, 2.22915785723627,
+                                                    0, 3.1144754240626, 3.72397173486482, 3.323669305973, 16.9176896774671,
+                                                    2.40921027994661, 1.41160848191838, 3.03517059385205))
+
+  # test flexmix snr
+  flexmix_test <- computeSNR(processed, update_correspondence = TRUE, package = "flexmix")
+  expect_equal(flexmix_test$CorrespondenceMatrix$snr, c(1, 1.46662215920315, 0, 0, 2.00872721014486, 0, 3.08641724395242,
+                                                       3.80602499772131, 3.31489921308867, 15.2477686105868, 2.40921027994661,
+                                                       1.41702657054023, 2.9132994473942))
 
 })
 
