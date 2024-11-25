c100cc5acead353eac715b84f633262f42516dad
John Abbey <89733453+JohnAbbey98@users.noreply.github.com>
2024-11-25 14:26:51 +0100

    Update R/computeSNR.R
    Co-authored-by: Leonardo Schwarz <leoschwarz@users.noreply.github.com>

--- R/computeSNR.R
+++ R/computeSNR.R
@@ -17,7 +17,7 @@
 #' raw <- readMSIData(path)
 #' pre <- preProcess(raw, cores = 2)
 #' peaks <- peakDetection(pre, core = 2)
-#' mmetapeaks <- generateMetapeaks(peaks, hist_smooth_factor = 1)
+#' metapeaks <- generateMetapeaks(peaks, hist_smooth_factor = 1)
 #' processed <- assignMetapeaks(metapeaks, pre = pre, refList = panel)
 
 computeSNR <- function(x, package = "mclust", update_correspondence = FALSE, q = 1) {
