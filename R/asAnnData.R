ae3898c039d2921df78aa16a3eed6f2090eaeb64
John Abbey <john@dqbm-nbm-joab.local>
2026-03-27 22:43:35 +0100

    fix anndata warning

--- R/asAnnData.R
+++ R/asAnnData.R
@@ -32,7 +32,7 @@ asAnnData <- function(x){
   obs_data <- coords
 
   # create anndata object
-  output <- anndata::AnnData(X = exp_data,
+  output <- AnnData(X = exp_data,
                              obs = data.frame(obs_data),
                              var = data.frame(var_data))
 
